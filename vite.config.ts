19faae10ef4aa304512bf72052f32184bd573182
RIKKAEBI <135724197+RIKKAEBI@users.noreply.github.com>
2025-02-13 15:28:04 +0200

    httpsで開発サーバーが開くように

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,19 @@ import { cloudflareDevProxy } from "@react-router/dev/vite/cloudflare";
 import autoprefixer from "autoprefixer";
 import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
+import fs from "fs";
+import path from "path";
 
 export default defineConfig(() => ({
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, "certificates/server.key")),
+      cert: fs.readFileSync(path.resolve(__dirname, "certificates/server.crt")),
+    },
+    host: "local.kotohiro.com",
+    port: 3000,
+    proxy: {},
+  },
   define: {
     BASE_URL: `${JSON.stringify("https://www.kotohiro.com")}`,
     API_BASE_URL: `${JSON.stringify("https://api.kotohiro.com")}`,
