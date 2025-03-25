9054c5e19ff6000c8fddd6093b5362dc1dea2cd5
RIKKAEBI <135724197+RIKKAEBI@users.noreply.github.com>
2025-03-25 15:40:37 +0200

    🚧 UIだけでも使えるように

--- app/components/Input/index.tsx
+++ app/components/Input/index.tsx
@@ -6,7 +6,7 @@ type Props = ComponentProps<"input"> & {
   icon?: string;
 };
 
-const input = tv({
+export const input = tv({
   base: "h-12 w-full rounded-md border border-gray-300 bg-white px-2 text-sm",
   variants: {
     error: { true: "border-red-500 bg-red-100" },
