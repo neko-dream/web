0525eac75e0459df47290e18de4b3195bb5b88ba
RIKKAEBI <135724197+RIKKAEBI@users.noreply.github.com>
2025-03-25 15:40:10 +0200

    🔥 不要な検索項目を削除

--- app/routes/_pages.home/modules/loader.ts
+++ app/routes/_pages.home/modules/loader.ts
@@ -12,58 +12,15 @@ const setStatus = (requestURL: string) => {
   }
 };
 
-const setSortKey = (requestURL: string) => {
-  try {
-    const query = new URL(requestURL).searchParams.get("sortKey");
-    if (
-      query === "latest" ||
-      query === "oldest" ||
-      query === "mostReplies" ||
-      query === "nearest"
-    ) {
-      return query;
-    }
-  } catch {
-    return;
-  }
-
-  return undefined;
-};
-
 const setTheme = (requestURL: string) => {
   try {
     const query = new URL(requestURL).searchParams.get("q");
-    if (
-      query !== "latest" &&
-      query !== "oldest" &&
-      query !== "mostReplies" &&
-      query !== "open" &&
-      query !== "finished"
-    ) {
+    if (query !== "open" && query !== "finished") {
       return query;
     }
   } catch {
     return;
   }
-
-  return undefined;
-};
-
-const setGeoLocation = (requestURL: string) => {
-  try {
-    const lat = new URL(requestURL).searchParams.get("lat");
-    const lng = new URL(requestURL).searchParams.get("lng");
-    if (lat && lng) {
-      return {
-        lat: Number(lat),
-        lng: Number(lng),
-      };
-    }
-  } catch {
-    return;
-  }
-
-  return undefined;
 };
 
 export const loader = ({ request }: LoaderFunctionArgs) => {
@@ -73,14 +30,13 @@ export const loader = ({ request }: LoaderFunctionArgs) => {
       params: {
         query: {
           status: setStatus(request.url),
-          sortKey: setSortKey(request.url),
           theme: setTheme(request.url),
-          latitude: setGeoLocation(request.url)?.lat,
-          longitude: setGeoLocation(request.url)?.lng,
         },
       },
     })
     .then((res) => res?.data || null);
+
+  $session.then(console.log);
 
   return { $session };
 };
