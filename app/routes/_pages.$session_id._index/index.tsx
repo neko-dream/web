29f386ca8108eafd37dcaded664facb86069381d
RIKKAEBI <135724197+RIKKAEBI@users.noreply.github.com>
2025-03-30 14:52:42 +0300

    👍 コンポーネントの置き換え

--- app/routes/_pages.$session_id._index/index.tsx
+++ app/routes/_pages.$session_id._index/index.tsx
@@ -1,10 +1,9 @@
 import { useOutletContext } from "react-router";
 import { SessionRouteContext } from "../_pages.$session_id/types";
-import xss from "xss";
+import { SanitizedHtml } from "~/features/xss-html";
 
 export default function Page() {
   const { session } = useOutletContext<SessionRouteContext>();
-  const sanitizedHtml = xss(session.description || "");
 
-  return <article dangerouslySetInnerHTML={{ __html: sanitizedHtml || "" }} />;
+  return <SanitizedHtml html={session.description || ""} />;
 }
