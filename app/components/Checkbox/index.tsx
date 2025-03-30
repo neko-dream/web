5a9dc99cf837c372b494d29ba7b0b0ae6393db11
RIKKAEBI <135724197+RIKKAEBI@users.noreply.github.com>
2025-03-30 14:48:20 +0300

    👍 背景を白に

--- app/components/Checkbox/index.tsx
+++ app/components/Checkbox/index.tsx
@@ -11,7 +11,7 @@ export const Checkbox = ({ id, label, ...props }: Props) => {
         id={id}
         type="checkbox"
         {...props}
-        className="relative h-[20px] w-[20px] cursor-pointer appearance-none rounded border border-[#CED4DA] checked:bg-[#4dabf7] [&:checked::after]:absolute [&:checked::after]:top-[5px] [&:checked::after]:left-[3px] [&:checked::after]:h-[6px] [&:checked::after]:w-[12px] [&:checked::after]:-rotate-45 [&:checked::after]:transform [&:checked::after]:border-b-2 [&:checked::after]:border-l-2 [&:checked::after]:border-white [&:checked::after]:content-['']"
+        className="relative h-[20px] w-[20px] cursor-pointer appearance-none rounded border border-[#CED4DA] bg-white checked:bg-[#4dabf7] [&:checked::after]:absolute [&:checked::after]:top-[5px] [&:checked::after]:left-[3px] [&:checked::after]:h-[6px] [&:checked::after]:w-[12px] [&:checked::after]:-rotate-45 [&:checked::after]:transform [&:checked::after]:border-b-2 [&:checked::after]:border-l-2 [&:checked::after]:border-white [&:checked::after]:content-['']"
       />
       <label htmlFor={id} className="text-sm font-medium text-gray-900">
         {label}
