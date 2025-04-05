5c62646e07676875315f6ec2174f5547459170dd
RIKKAEBI <135724197+RIKKAEBI@users.noreply.github.com>
2025-04-05 09:27:04 +0300

    👍 OGP画像を変更

--- app/routes/_pages/modules/meta.ts
+++ app/routes/_pages/modules/meta.ts
@@ -3,5 +3,5 @@ import { generateMetaTag } from "~/utils/generateMetaTag";
 export const meta = generateMetaTag({
   title: "ことひろ",
   description: "多種多様な意見や言葉を重ねてよりよい意思決定を目指すサービス",
-  ogp: "/ogp.png",
+  ogp: "/ogp.webp",
 });
