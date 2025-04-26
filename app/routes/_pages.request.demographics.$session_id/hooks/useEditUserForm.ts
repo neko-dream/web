af993e5159196f69852e29d481a911b102aab243
RIKKAEBI <135724197+RIKKAEBI@users.noreply.github.com>
2025-04-26 14:15:34 +0900

    👍 デモグラリクエスト画面でも誕生日を入れれるように

--- app/routes/_pages.request.demographics.$session_id/hooks/useEditUserForm.ts
+++ app/routes/_pages.request.demographics.$session_id/hooks/useEditUserForm.ts
@@ -1,3 +1,4 @@
+import dayjs from "dayjs";
 import { useNavigate } from "react-router";
 import { toast } from "react-toastify";
 import { useCustomForm } from "~/hooks/useCustomForm";
@@ -23,6 +24,9 @@ export const useEditUserForm = ({ user, sessionID, returnPage }: Props) => {
         body: {
           ...value,
           icon: undefined,
+          dateOfBirth: dayjs(value.dateOfBirth).format(
+            "YYYYMMDD",
+          ) as unknown as number,
         },
       });
 
