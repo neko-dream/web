cca0c67058e09b4739e4d1855fca6a67737e8fa4
RIKKAEBI <135724197+RIKKAEBI@users.noreply.github.com>
2025-06-01 13:52:48 +0900

    👍 結論投稿のメッセージを送れるように

--- app/routes/_pages.make.$session_id.loadmap/index.tsx
+++ app/routes/_pages.make.$session_id.loadmap/index.tsx
@@ -1,7 +1,28 @@
-export default function Page() {
+import { RichTextEditor } from "~/components/features/rich-text-editor";
+import { Heading } from "~/components/ui/heading";
+import type { Route } from "../_pages.make.$session_id.loadmap/+types";
+
+export { loader } from "./modules/loader";
+export { meta } from "./modules/meta";
+
+export default function Page({
+  loaderData: { session },
+}: Route.ComponentProps) {
+  const handleImageUploader = async (file: File) => {
+    await file;
+    return "";
+  };
+
   return (
-    <div>
-      <p>make loadmap</p>
+    <div className="flex-1 bg-cs-gray-200">
+      <Heading title="活動報告" to={`/${session.id}`} isLink={true} />
+
+      <form className="mx-auto w-full max-w-xl p-4">
+        <RichTextEditor
+          onImageLoad={handleImageUploader}
+          onUpdate={console.log}
+        />
+      </form>
     </div>
   );
 }
