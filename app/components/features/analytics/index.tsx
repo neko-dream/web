31ce572c929b60ed77764f90451a2f6f76084083
RIKKAEBI <135724197+RIKKAEBI@users.noreply.github.com>
2026-02-04 13:28:02 +0000

    🚑 Analyticsのハイドレーションエラーを修正

--- app/components/features/analytics/index.tsx
+++ app/components/features/analytics/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router";
 
 declare global {
@@ -11,8 +11,10 @@ const ID = "G-G9K1SZJ553"; // Google AnalyticsのID
 
 export default function Analytics() {
   const { pathname } = useLocation();
+  const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
+    setIsClient(true);
     if (!window.dataLayer) {
       window.dataLayer = [];
     }
@@ -34,20 +36,21 @@ export default function Analytics() {
           style={{ display: "none", visibility: "hidden" }}
         />
       </noscript>
-      <script
-        async={true}
-        suppressHydrationWarning={true}
-        // biome-ignore lint/security/noDangerouslySetInnerHtml: Google Tag Manager script
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer', '${ID}');
-          `,
-        }}
-      />
+      {isClient && (
+        <script
+          async={true}
+          // biome-ignore lint/security/noDangerouslySetInnerHtml: Google Tag Manager script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer', '${ID}');
+            `,
+          }}
+        />
+      )}
     </>
   );
 }
